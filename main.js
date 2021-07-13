51c3f8ee427428f004a33c4f9a96b7b606e98df7
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-07-12 21:09:38 -0400

    more JS work

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-// const a1 = document.querySelector('.a1');
-
-// const activeState = document.querySelector('.a1a');
-
-// const toggle = () => {
-//     activeState.classList.toggle('visible');
-// };
-
-// a1.addEventListener('click', toggle);
-
-
-
-
+//works!!
 const arrow = document.querySelector('.arrow');
 
 const activeState = document.querySelector('.active-state');
 
+const question = document.querySelector('q1');
+
 const toggle = () => {
     activeState.classList.toggle('visible');
+    question.classList.toggle('visible');
 };
 
 arrow.addEventListener('click', toggle);
+
+//arr2
+const arr2 = document.querySelector('.arr2');
+const as2 = document.querySelector('.ans2');
+
+const t2 = () => {
+    as2.classList.toggle('visible');
+};
+
+arr2.addEventListener('click', t2);
